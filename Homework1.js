94d6a87cbc27951275a46a92b2611ffee0a5f7d2
mozg3000 <i.luka@outlook.com>
2018-12-20 16:21:50 +0300

    Д.З. №1 1.0.0

---
+++ Homework1.js
@@ -0,0 +1,6 @@
+// JavaScript source code
+var T=prompt("Укажите температуру по Фарингейту, °F", 0);
+T=(9 / 5) * T+32;
+var name="Василий",admin=name;
+var x = 1000+"108";
+alert("1. Значение температуры в градусах Цельсия, °C = " + T + "\n2. admin = "+admin+"\n3. 1000+'108' = "+x);
